af42c6d6605452eb9a32108ba637d0544d941808
Celia Cintas <cintas.celia@gmail.com>
2017-10-06 15:14:14 -0300

    nvidia running py3

--- run_docker_gpu.sh
+++ run_docker_gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
-nvidia-docker run -t -i -p 8000:8888 --rm -v $(pwd):/star_wars_hackathon pablo1n7/tatooine:latest
+nvidia-docker run -t -i -p 8000:8888 --rm -v $(pwd):/star_wars_hackathon pablo1n7/tatooine:3.4
